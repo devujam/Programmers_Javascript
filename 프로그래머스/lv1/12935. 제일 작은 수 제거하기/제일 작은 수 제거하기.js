6f8733c78d7180676367dd06bba7b503d2b7d55f
우재민 <ujamdev@gmail.com>
2022-07-02 17:48:41 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.05 ms, Memory: 30.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    let answer;
+    if(arr.length == 1) {
+        return [-1];
+    } else {
+        arr.splice(arr.indexOf(Math.min(...arr)), 1)
+        answer = arr;
+    }
+    return answer;
+}
