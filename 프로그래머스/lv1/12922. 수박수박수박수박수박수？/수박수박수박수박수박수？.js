6392146337de2135ba1b7fe11156effd33632d02
Jaemin Woo <ujamdev@gmail.com>
2022-07-04 22:44:25 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,8 +1,7 @@
 function solution(n) {
     let answer = "";
     for(let i = 1; i <= n; i++) {
-        if(i % 2 === 0) answer += "박";
-        else answer += "수";
+        i % 2 === 0 ? answer += "박" : answer += "수"
     }
     return answer
 }
