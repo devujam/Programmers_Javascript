dd30842cd18d8a08e2a1bc609172330257b5d181
Jaemin Woo <ujamdev@gmail.com>
2022-07-06 23:57:51 +0900

    [Bronze V] Title: 시험 성적, Time: 136 ms, Memory: 7832 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+   input: process.stdin
+});
+
+rl.on('line', (line) => {
+    const input = line.split(' ');
+    
+    const a = Number(input[0]);
+    
+    if(a >= 90 && a <= 100) {
+        console.log('A');
+    } else if (a >= 80 && a <= 89) {
+        console.log('B');
+    } else if (a >= 70 && a <= 79) {
+        console.log('C');
+    } else if (a >= 60 && a <= 69) {
+        console.log('D');
+    } else {
+        console.log('F');
+    }
+    rl.close();
+}).on('close', () => {
+    process.exit();
+})
