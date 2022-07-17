027db42fd0ee4861eb1eadf9448f65da5a897d84
Jaemin Woo <ujamdev@gmail.com>
2022-07-17 11:42:34 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,10 +1,7 @@
 function solution(n) {
     let sqrt = Math.sqrt(n);
 
-    if (sqrt % 1 === 0) {
-        return Math.pow(sqrt+1,2); // 제곱근일 경우
-    } else {
-        return -1;
-    }
+    if (sqrt % 1 === 0) return Math.pow(sqrt+1,2); // 제곱근일 경우
+    else return -1;
     
 }
