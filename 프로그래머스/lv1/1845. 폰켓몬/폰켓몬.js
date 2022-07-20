126e562138fbcd0df807b12029676d74d534d922
Jaemin Woo <ujamdev@gmail.com>
2022-07-20 17:18:29 +0900

    [level 1] Title: 폰켓몬, Time: 0.25 ms, Memory: 30.1 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,10 +1,10 @@
 function solution(nums) {
     let answer = 0;
     
-    const count = nums.length / 2;
+    const max = nums.length / 2;
     const setArr = new Set(nums);
     const arr = [...setArr]
     const arrLength = arr.length;
     
-    return count > arrLength ? arrLength : count
+    return max > arrLength ? arrLength : max
 }
