f0b4d99a50623411604152fcdce7bde7848ef0cb
Jaemin Woo <ujamdev@gmail.com>
2022-08-24 21:25:20 +0900

    [Bronze V] Title: A-B, Time: 168 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split(" ");
+const num1 = parseInt(input[0]);
+const num2 = parseInt(input[1]);
+
+console.log(num1 - num2);
