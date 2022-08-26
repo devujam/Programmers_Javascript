c76f624787c064f163a146e8dd53850a16dcfc8b
Jaemin Woo <ujamdev@gmail.com>
2022-08-26 23:00:32 +0900

    [Bronze III] Title: 최댓값, Time: 112 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,6 +1,6 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(x=> Number(x));
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(el => Number(el));
 
-let max = Math.max(...input)
+const max = Math.max(...input);
 
-console.log(max)
-console.log(input.indexOf(max) + 1)
+console.log(max);
+console.log(input.indexOf(max) + 1);
