76f78481b9b78171e1bb7d022dc21f69707d782f
Jaemin Woo <ujamdev@gmail.com>
2023-01-08 11:43:22 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,16 +1,14 @@
 function solution(food) {
     let answer = '';
-    const arr = [];
     
     for(let i = 0; i < food.length; i++) {
         const foodRow = food[i].toString();
-        arr.push(food[i]);
-        const arrIndex = (arr.length - 1).toString();
+        const index = i.toString();
         if(Number(foodRow) > 1) {
             if(foodRow % 2 === 0) {
-                answer += arrIndex.repeat(Number(foodRow) / 2);
+                answer += index.repeat(Number(foodRow) / 2);
             } else {
-                answer += arrIndex.repeat((Number(foodRow) - 1) / 2);
+                answer += index.repeat((Number(foodRow) - 1) / 2);
             }
         }
     }
