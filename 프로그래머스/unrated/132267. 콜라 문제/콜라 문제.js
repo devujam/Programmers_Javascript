c9cf60eddb3dfda24215bad5843fb7d32dc6c428
Jaemin Woo <ujamdev@gmail.com>
2023-01-09 19:46:30 +0900

    [unrated] Title: 콜라 문제, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,21 +1,9 @@
 function solution(a, b, n) {
-    let answer = 0;
-    let temp = 0;
-    
-    while(n > 1) {
-        const newBottle = ((n + temp) / a) * b;
-        if((n + temp) % a === 0) {
-            console.log(n)
-            answer += Math.floor(newBottle);
-            n = (n / a) + Math.floor(newBottle);
-        } else {
-            console.log(n)
-            answer += Math.floor(newBottle);
-            temp = (n + temp) % a
-            n = (n / a) + Math.floor(newBottle);
-        }
+    let answer = 0
+    while(n >= a) {
+        const newCola = Math.floor(n / a) * b;
+        answer += newCola;
+        n = (n % a) + newCola;
     }
-    
-    
-    return answer;
+    return answer
 }
