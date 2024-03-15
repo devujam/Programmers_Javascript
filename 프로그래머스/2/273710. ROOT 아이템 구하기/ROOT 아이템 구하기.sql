4df25711869300f29ed7a4fc722dccc72970a7f5
Jaemin Woo <ujamdev@gmail.com>
2024-03-15 21:44:03 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -0,0 +1,6 @@
+SELECT itemInfo.ITEM_ID
+  , itemInfo.ITEM_NAME
+FROM ITEM_INFO itemInfo
+LEFT JOIN ITEM_TREE itemTree ON itemTree.ITEM_ID = itemInfo.ITEM_ID 
+WHERE itemTree.PARENT_ITEM_ID IS NULL
+ORDER BY itemInfo.ITEM_ID ASC
