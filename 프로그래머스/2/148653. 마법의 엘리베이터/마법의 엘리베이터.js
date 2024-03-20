a39b2608111aaa2b3fb5bcf544c42ee8b9580d1e
Jaemin Woo <ujamdev@gmail.com>
2024-03-20 15:39:55 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -0,0 +1,22 @@
+function solution(storey) {
+    let answer = 0;
+    
+    while(storey) {
+        let cur = storey % 10;
+        let next = (storey / 10) % 10;
+        
+        if(cur > 5) {
+            answer += 10 - cur;
+            storey += 10;
+        } else if(cur === 5) {
+            answer += cur;
+            storey += next >= 5 ? 10 : 0;
+        } else {
+            answer += cur;
+        }
+        
+        storey = Math.floor(storey / 10);
+    }
+    
+    return answer;
+}
